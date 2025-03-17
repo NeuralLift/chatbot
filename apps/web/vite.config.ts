f5bd3db8e91ab8dfd136505266aeb56c7e1b5f26
mgalihpp <muhammadgalih451@gmail.com>
2025-03-17 09:14:25 +0700

    fix: disable pwa

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,57 +1,58 @@
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+
+// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      scope: '/',
-      workbox: {
-        cleanupOutdatedCaches: true,
-      },
-      manifest: {
-        orientation: 'any',
-        display: 'standalone',
-        lang: 'en-US',
-        name: 'NeuralLift AI',
-        short_name: 'NeuralLift',
-        start_url: '/',
-        description: '24/7 Customer Support Powered by AI',
-        background_color: '#ffffff',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-        ],
-      },
-      injectRegister: 'auto',
-      devOptions: {
-        enabled: true,
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   scope: '/',
+    //   workbox: {
+    //     cleanupOutdatedCaches: true,
+    //   },
+    //   manifest: {
+    //     orientation: 'any',
+    //     display: 'standalone',
+    //     lang: 'en-US',
+    //     name: 'NeuralLift AI',
+    //     short_name: 'NeuralLift',
+    //     start_url: '/',
+    //     description: '24/7 Customer Support Powered by AI',
+    //     background_color: '#ffffff',
+    //     theme_color: '#ffffff',
+    //     icons: [
+    //       {
+    //         src: 'pwa-64x64.png',
+    //         sizes: '64x64',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: 'pwa-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: 'maskable-icon-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'maskable',
+    //       },
+    //     ],
+    //   },
+    //   injectRegister: 'auto',
+    //   devOptions: {
+    //     enabled: true,
+    //   },
+    // }),
   ],
   resolve: {
     alias: {
